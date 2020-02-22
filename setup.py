1cfde552dd2a8a4aecec7f7b2c719fbd4be16d62
Alen Komic <akomic@users.noreply.github.com>
2020-02-22 17:09:04 +0100

    Bugfix

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(name='ZabbixProto',
-      version='0.0.1',
+      version='v0.0.1',
       description='Zabbix Communication Protocols',
       long_description=open('README.md', 'r').read() + '\n\n' + open(
           'CHANGELOG.md', 'r').read(),
       author='Alen Komic',
       author_email='akomic@gmail.com',
       url='https://github.com/akomic/python-zabbix-proto',
-      download_url='https://github.com/akomic/python-zabbix-proto/archive/0.0.1.tar.gz',
+      download_url='https://github.com/akomic/python-zabbix-proto/archive/v0.0.1.tar.gz',
       packages=['zabbixproto'],
       install_requires=[
           'datetime',
@@ -21,7 +21,7 @@ setup(name='ZabbixProto',
       keywords='Zabbix Protocols Sender Proxy Agent',
       license='Apache Software License',
       classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
+          'Development Status :: 2 - Alpha',
           'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python :: 3',
           'Topic :: System :: Monitoring',
